768fd3a637836305b13f9ae2e202619b0c87719d
nmhn0111 <nmhn0111@gmail.com>
2022-08-03 03:21:25 +0900

    인스타그램 완성!

--- pjs.js
+++ pjs.js
@@ -1,23 +1,15 @@
 document.addEventListener("DOMContentLoaded",
     function(e){
-        let nameEditButton = document.querySelector("#nameEditButton")
-        let editName = document.querySelector("#editNameField")
-        let okButton = document.querySelector("#okButton")
-        let newName = document.querySelector("#newName")
-        let nameHere = document.querySelector("#nameHere")
-
-        nameEditButton.addEventListener("click",
+        let wow = document.querySelector("#wow")
+        let button = document.querySelector("input")
+        let _wow = wow.textContent
+        
+        button.addEventListener("click",
             function(e){
-                editName.style.display = "block"
-                nameEditButton.style.display = "none"
-            }
-        )
-
-        okButton.addEventListener("click",
-            function(e){
-                nameHere.textContent = newName.value
-                editName.style.display = "none"
-                nameEditButton.style.display = "inline"
+                console.log(typeof(_wow))
+                console.log(_wow)
+                wow.innerHTML = "<input value=" + _wow + "></input>"
+                console.log(wow)
             }
         )
     }
